627b3ec322318b3e372453067609c97757a39305
PaulKim098 <paul.hkim@outlook.com>
2022-10-24 17:31:04 -0500

    inital push

---
+++ src/MergeConflict.java
@@ -0,0 +1,8 @@
+public class MergeConflict {
+    public static void main(String[] args) {
+        System.out.println("We like coding and git is fun")
+        System.out.println("We added another line of code");;
+        System.out.println("We added another line of code");;
+        System.out.println("We added another line of code");;
+    }
+}
